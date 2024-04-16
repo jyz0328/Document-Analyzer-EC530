b430571a5fe256fa0fe0e83a98f90bb261f49db8
jyz0328 <144707485+jyz0328@users.noreply.github.com>
2024-04-15 23:08:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #先开 后端进行 python3 ./app.py
 #再单开一个terminal 输入python3 ./nowclient.py 前端运行
 #按control c退出后端
+#example username and password:jgqmxjpy和ZPbImfSp
 # app.py
 from flask import Flask
 from flask_restful import Api
